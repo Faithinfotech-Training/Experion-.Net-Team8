7b2eb035bbefca867d22b321d477a6ae3d8afb71
vyshnav.sasidharan <vyshnav.sasidharan@VYSHNAVSASIDHAR>
2021-11-26 17:42:57 +0530

    Employee controller and model created

---
+++ Repository/EmployeeRepo.cs
@@ -0,0 +1,37 @@
+﻿using Clinic_Management_System_8.Models;
+using Clinic_Management_System_8.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Repository
+{
+    public class EmployeeRepo : IEmlpoyeeRepo
+    {
+        public Task<Employees> AddEmployee(Employees employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employees> DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EmployeeModel> GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<EmployeeModel>> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employees> UpdateEmployee(Employees employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/IEmlpoyeeRepo.cs
@@ -0,0 +1,29 @@
+﻿using Clinic_Management_System_8.Models;
+using Clinic_Management_System_8.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Repository
+{
+    public interface IEmlpoyeeRepo
+    {
+        //--- get all employees ---//
+        Task<List<EmployeeModel>> GetEmployees();
+
+        //--- get employee by id ---//
+        Task<EmployeeModel> GetEmployeeById(int id);
+
+        //--- add Employee ---//
+        Task<Employees> AddEmployee(Employees employee);
+
+
+        //--- update Employee ---//
+        Task<Employees> UpdateEmployee(Employees employee);
+
+        //--- delete Employee by id---//
+
+        Task<Employees> DeleteEmployee(int id);
+    }
+}

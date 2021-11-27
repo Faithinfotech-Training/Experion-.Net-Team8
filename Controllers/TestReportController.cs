5941c8babfffa13ebe2b488dcdeadc94186e8f37
surabhip.s444@gmail.com <surabhip.s444@gmail.com>
2021-11-27 23:12:53 +0530

    Test report

---
+++ Controllers/TestReportController.cs
@@ -0,0 +1,150 @@
+﻿using Clinic_Management_System_8.Models;
+using Clinic_Management_System_8.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestReportController : ControllerBase
+    {
+        ITestReport postRepository;
+        public TestReportController(ITestReport _p)
+        {
+            postRepository = _p;
+        }
+        #region Get test report
+
+        [HttpGet]
+        //[Authorize]
+        public async Task<IActionResult> GetTestReport()
+        {
+            try
+            {
+                var posts = await postRepository.GetTestReport();
+                if (posts == null)
+                {
+                    return NotFound();
+                }
+                return Ok(posts);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+        }
+        #endregion
+        #region Add test report
+        [HttpPost]
+        //[Authorize]
+        public async Task<IActionResult> AddTestReport(TestReports test)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var postId = await postRepository.AddTestReport(test);
+                    if (postId>0)
+                    {
+                        return Ok(postId);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+
+        #endregion
+        #region update test report
+        [HttpPut]
+        //[Authorize]
+        public async Task<IActionResult> UpdateTestReport(TestReports test)
+        {
+            //Check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await postRepository.UpdateTestReport(test);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+        #region Getreport by id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTestReportById(int id)
+        {
+            try
+            {
+                var post = await postRepository.GetTestReportById(id);
+                if (post != null)
+                {
+                    return Ok(post);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+        #region Getreport by date
+        [HttpGet("{date}")]
+        public async Task<IActionResult> GetTestReportByDate(DateTime date)
+        {
+            try
+            {
+                var post = await postRepository.GetTestReportByDate(date);
+                if (post != null)
+                {
+                    return Ok(post);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTestReport(int id)
+        {
+            //Check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await postRepository.DeleteTestReport(id);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Repository/TestReport.cs
@@ -0,0 +1,118 @@
+﻿using Clinic_Management_System_8.Models;
+using Clinic_Management_System_8.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Repository
+{
+    public class TestReport : ITestReport
+    {
+        CMSContext _db;
+
+        public TestReport(CMSContext db)
+        {
+            _db = db;
+        }
+        //--Add 
+        public async Task<int> AddTestReport(TestReports test)
+        {
+            if (_db != null)
+            {
+                await _db.AddAsync(test);
+                await _db.SaveChangesAsync();
+                return test.TestReportId;
+            }
+            return 0;
+        }
+
+        public async Task DeleteTestReport(int id)
+        {
+            //--- locating Testreport by id ---//
+            var test = _db.TestReports.FirstOrDefault(e => e.TestReportId == id);
+            if (test != null)
+            {
+                _db.TestReports.Remove(test);
+                await _db.SaveChangesAsync();
+
+
+            }
+        }
+            public async Task<List<LabReportModel>> GetTestReport()
+            {
+                if (_db != null)
+                {
+                    //LINQ
+                    return await (from t in _db.TestReports
+                                  from e in _db.Employees
+                                  from p in _db.Patients
+                                  where t.DoctorId == e.EmployeeId && t.LabTechnicianId == e.EmployeeId && t.PatientId == p.PatientId
+                                  select new LabReportModel
+                                  {
+                                      TestReportId = t.TestReportId,
+                                      TestReport = t.TestReport,
+                                      PatientName = p.PatientName,
+                                      DoctorName = e.EmployeeName,
+                                      LabTechnicianName = e.EmployeeName,
+                                      ReportGeneratedDate = t.ReportGeneratedDate
+                                  }).ToListAsync();
+                }
+                return null;
+            }
+
+        public  async Task<LabReportModel> GetTestReportByDate(DateTime date)
+        {
+            if (_db != null)
+            {
+                //LINQ
+                return await (from t in _db.TestReports
+                              from e in _db.Employees
+                              from p in _db.Patients
+                              where t.DoctorId == e.EmployeeId && t.LabTechnicianId == e.EmployeeId && t.PatientId == p.PatientId && t.ReportGeneratedDate==date
+                              select new LabReportModel
+                              {
+                                  TestReportId = t.TestReportId,
+                                  TestReport = t.TestReport,
+                                  PatientName = p.PatientName,
+                                  DoctorName = e.EmployeeName,
+                                  LabTechnicianName = e.EmployeeName,
+                                  ReportGeneratedDate = t.ReportGeneratedDate
+                              }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+
+        public async Task<LabReportModel> GetTestReportById(int id)
+        {
+            if (_db != null)
+            {
+                //LINQ
+                return await(from t in _db.TestReports
+                             from e in _db.Employees
+                             from p in _db.Patients
+                             where t.DoctorId == e.EmployeeId && t.LabTechnicianId == e.EmployeeId && t.PatientId == p.PatientId && t.TestReportId==id
+                             select new LabReportModel
+                             {
+                                 TestReportId = t.TestReportId,
+                                 TestReport = t.TestReport,
+                                 PatientName = p.PatientName,
+                                 DoctorName = e.EmployeeName,
+                                 LabTechnicianName = e.EmployeeName,
+                                 ReportGeneratedDate = t.ReportGeneratedDate
+                             }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+
+        public async Task UpdateTestReport(TestReports test)
+        {
+            if (_db != null)
+            {
+                _db.TestReports.Update(test);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}

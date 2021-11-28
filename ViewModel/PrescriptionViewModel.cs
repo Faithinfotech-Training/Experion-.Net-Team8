6352c64d09fe2ff808c7544d591a1636de42aabd
surabhip.s444@gmail.com <surabhip.s444@gmail.com>
2021-11-28 14:31:49 +0530

    Prescription added

---
+++ ViewModel/PrescriptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.ViewModel
+{
+    public class PrescriptionViewModel
+    {
+        public int PrescriptionId { get; set; }
+        public string Prescription { get; set; }
+        public DateTime PrescriptionDate { get; set; }
+        public string DoctorName{ get; set; }
+        public string  PatientName { get; set; }
+
+    }
+}

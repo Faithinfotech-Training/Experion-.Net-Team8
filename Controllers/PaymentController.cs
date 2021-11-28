52f3f5910853356a6c290734d24cad49c71b728b
surabhip.s444@gmail.com <surabhip.s444@gmail.com>
2021-11-28 21:54:54 +0530

    Payment details

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,113 @@
+﻿using Clinic_Management_System_8.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        IPayment pay ;
+        public PaymentController(IPayment _p)
+        {
+            pay = _p;
+        }
+        #region Get payment details
+
+        [HttpGet]
+        //[Authorize]
+        public async Task<IActionResult> GetPaymentDetails()
+        {
+            try
+            {
+                var posts = await pay.GetPaymentDetails();
+                if (posts == null)
+                {
+                    return NotFound();
+                }
+                return Ok(posts);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+
+        }
+        #endregion
+        #region Add payment details
+        [HttpPost]
+        //[Authorize]
+        public async Task<IActionResult> AddPayment(Payments payment)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var postId = await pay.AddPayment(payment);
+                    if (postId > 0)
+                    {
+                        return Ok(postId);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+
+        #endregion
+        #region update payment details
+        [HttpPut]
+        //[Authorize]
+        public async Task<IActionResult> UpdatePayment(Payments payment)
+        {
+            //Check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await pay.UpdatePayment(payment);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+        #region Get payment by patient id 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPaymentByPatientId(int id)
+        {
+            try
+            {
+                var post = await pay.GetPaymentByPatientId(id);
+                if (post != null)
+                {
+                    return Ok(post);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+        
+    }
+}
---
+++ Repository/Payment.cs
@@ -0,0 +1,88 @@
+﻿using Clinic_Management_System_8.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic_Management_System_8.Models
+{
+    public class Payment:IPayment
+    {
+        CMSContext _db;
+
+        public Payment(CMSContext db)
+        {
+            _db = db;
+        }
+        #region Add Payment
+
+        public async  Task<int> AddPayment(Payments payment)
+        {
+            if (_db != null)
+            {
+                await _db.Payments.AddAsync(payment);
+                await _db.SaveChangesAsync();
+                return payment.PaymentId;
+            }
+            return 0;
+        }
+        #endregion
+        #region Get payment
+        public async Task<List<PaymentViewModel>> GetPaymentDetails()
+        {
+            //--- get  all payments ---//
+
+            if (_db != null)
+            {
+                //LINQ
+                return await(from p in _db.Payments
+                             from t in _db.Patients
+                             where p.PatientId==t.PatientId
+                             select new PaymentViewModel
+                             {
+                                 PaymentId=p.PaymentId,
+                                 Amount=p.Amount,
+                                 PatientName=t.PatientName,
+                                 PaymentDate=p.PaymentDate
+                             }).ToListAsync();
+            }
+            return null;
+        }
+        #endregion
+        #region Get payment details by patient id
+
+       
+        public async Task<PaymentViewModel> GetPaymentByPatientId(int id)
+        {
+            //--- get payment by patient id   ---//
+
+            if (_db != null)
+            {
+                //LINQ
+                return await(from p in _db.Payments
+                             from t in _db.Patients
+                             where p.PatientId == t.PatientId && p.PatientId==id
+                             select new PaymentViewModel
+                             {
+                                 PaymentId = p.PaymentId,
+                                 Amount = p.Amount,
+                                 PatientName = t.PatientName,
+                                 PaymentDate = p.PaymentDate
+                             }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+        #endregion
+        #region Update payment      
+        public async Task UpdatePayment(Payments payment)
+        {
+            if (_db != null)
+            {
+                _db.Payments.Update(payment);
+                await _db.SaveChangesAsync();
+            }
+        }
+        #endregion
+    }
+}

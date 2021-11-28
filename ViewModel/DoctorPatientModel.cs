f6b941ba76a330a265bd8a1ad9a288f243ce4682
Avin Joseph <avin.joseph@experionglobal.com>
2021-11-28 17:30:15 +0530

    changes commited

--- ViewModel/DoctorPatientModel.cs
+++ ViewModel/DoctorPatientModel.cs
@@ -7,5 +7,15 @@ namespace Clinic_Management_System_8.ViewModel
 {
     public class DoctorPatientModel
     {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int PatientId { get; set; }
+        public string PatientName { get; set; }
+        public int Age { get; set; }
+        public decimal MobileNo { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }

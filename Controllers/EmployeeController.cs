188121325ca37427553874a197ba904e3ea40de9
vyshnav.sasidharan <vyshnav.sasidharan@VYSHNAVSASIDHAR>
2021-11-29 09:29:37 +0530

    employee controller completed

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Clinic_Management_System_8.Models;
+using Clinic_Management_System_8.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,145 @@ namespace Clinic_Management_System_8.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StaffController : ControllerBase
+    public class EmployeeController : ControllerBase
     {
+        private IEmlpoyeeRepo employeeRepo;
+
+        //--- Parameterized constructor ---//
+        public EmployeeController(IEmlpoyeeRepo _employeeRepo)
+        {
+            employeeRepo = _employeeRepo;
+        }
+
+
+        //--- Get all Employees ---//
+        #region GetEmployees
+
+        [HttpGet]
+        //[Authorize]
+        [Route("GetEmployees")]
+        public async Task<IActionResult> GetEmployees()
+        {
+            try
+            {
+                var employees = await employeeRepo.GetEmployees();
+                if (employees != null)
+                {
+                    return Ok(employees);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        #endregion
+
+
+        //--- Get  Employee by id ---//
+        #region GetEmployeeById
+
+        [HttpGet]
+        //[Authorize]
+        [Route("GetEmployeeById")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            try
+            {
+                var employee = await employeeRepo.GetEmployeeById(id);
+                if (employee != null)
+                {
+                    return Ok(employee);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        #endregion
+
+        //--- add a new employee ---//
+        #region AddEmployee
+        [HttpPost]
+        [Route("AddEmployee")]
+        public async Task<IActionResult> AddEmployee(Employees employee)
+        {
+            //check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var newEmployee = await employeeRepo.AddEmployee(employee);
+                    if (newEmployee != null)
+                    {
+                        return Ok(newEmployee);
+
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+
+
+        //--- update a Employee ---//
+        #region UpdateEmployee
+
+        [HttpPost]
+        [Route("UpdateEmployee")]
+        public async Task<IActionResult> UpdateEmployee([FromBody] Employees employee)
+        {
+            //Check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await employeeRepo.UpdateEmployee(employee);
+                    return Ok(employee);
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+
+        #region delete user 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deleteuser(int id)
+        {
+            //Check the validation of body
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await employeeRepo.DeleteEmployee(id);
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+        #endregion
+
+
     }
 }
--- Repository/EmployeeRepo.cs
+++ Repository/EmployeeRepo.cs
@@ -1,5 +1,6 @@
 ﻿using Clinic_Management_System_8.Models;
 using Clinic_Management_System_8.ViewModel;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,124 @@ namespace Clinic_Management_System_8.Repository
 {
     public class EmployeeRepo : IEmlpoyeeRepo
     {
-        public Task<Employees> AddEmployee(Employees employee)
+        private CMSContext db;
+
+        //Dependency Injection
+        //--- Parameterized Constructor ---//
+        public EmployeeRepo(CMSContext _db)
         {
-            throw new NotImplementedException();
+            db = _db;
         }
 
-        public Task<Employees> DeleteEmployee(int id)
+        //--- member function to add a new employee ---//
+        #region AddEmployee
+        public async Task<Employees> AddEmployee(Employees employee)
         {
-            throw new NotImplementedException();
+            if (db != null)
+            {
+                await db.Employees.AddAsync(employee);
+                await db.SaveChangesAsync();
+                return employee;
+            }
+            return null;
         }
+        #endregion
 
-        public Task<EmployeeModel> GetEmployeeById(int id)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task<List<EmployeeModel>> GetEmployees()
+        //--- member function to delete a employee ---//
+        #region DeleteEmployee
+        public async Task DeleteEmployee(int id)
         {
-            throw new NotImplementedException();
-        }
+            //--- locating employee by id ---//
 
-        public Task<Employees> UpdateEmployee(Employees employee)
-        {
-            throw new NotImplementedException();
+            Employees employee = db.Employees.FirstOrDefault(eid => eid.EmployeeId == id);
+            if (employee != null)
+            {
+                employee.IsActive = false;
+                await db.SaveChangesAsync();
+            }
         }
+        #endregion
+
+
+        //--- member function to get employee by id ---//
+        #region GetEmployeeById
+
+        public async Task<EmployeeModel> GetEmployeeById(int id)
+        {
+            if (db != null)
+            {
+                return await (from e in db.Employees
+                              from r in db.Roles
+                              from d in db.Departments
+                              from es in db.EmployeeSpecializations
+                              from s in db.Specializations
+
+                              where e.EmployeeId == id && e.RoleId == r.RoleId && e.DepartmentId == d.DepartmentId && e.EmployeeId == es.EmployeeId && es.SpecializationId == s.SpecializationId
+                              select new EmployeeModel
+                              {
+                                  EmployeeId = e.EmployeeId,
+                                  EmployeeName = e.EmployeeName,
+                                  Age = e.Age,
+                                  MobileNo = e.MobileNo,
+                                  Gender = e.Gender,
+                                  DateOfJoining = e.DateOfJoining,
+                                  Specialization = s.SpecializationName,
+                                  DepartmentName = d.DepartmentName,
+                                  RoleName = r.RoleName,
+                                  IsActive = e.IsActive
+                              }).FirstOrDefaultAsync();
+            }
+            return null;
+        }
+        #endregion
+
+
+        //--- member function to get employees ---//
+        #region GetEmployees
+        public async Task<List<EmployeeModel>> GetEmployees()
+        {
+            if (db != null)
+            {
+                return await(from e in db.Employees
+                             from r in db.Roles
+                             from d in db.Departments
+                             from es in db.EmployeeSpecializations
+                             from s in db.Specializations
+
+                             where e.RoleId == r.RoleId && e.DepartmentId == d.DepartmentId && e.EmployeeId == es.EmployeeId && es.SpecializationId == s.SpecializationId
+                             select new EmployeeModel
+                             {
+                                 EmployeeId = e.EmployeeId,
+                                 EmployeeName = e.EmployeeName,
+                                 Age = e.Age,
+                                 MobileNo = e.MobileNo,
+                                 Gender = e.Gender,
+                                 DateOfJoining = e.DateOfJoining,
+                                 Specialization = s.SpecializationName,
+                                 DepartmentName = d.DepartmentName,
+                                 RoleName = r.RoleName,
+                                 IsActive = e.IsActive
+                             }).ToListAsync();
+            }
+            return null;
+        }
+        #endregion
+
+
+        //--- member function to update a employee ---//
+        #region UpdateEmployee
+        public async Task<Employees> UpdateEmployee(Employees employee)
+        {
+            if (db != null)
+            {
+                db.Employees.Update(employee);
+                await db.SaveChangesAsync();
+                return employee;
+            }
+            return null;
+        }
+        #endregion
+
     }
 }

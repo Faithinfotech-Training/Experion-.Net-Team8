1ee5c9f6dd5a88064e44535db2f25aed4c69b1c8
Avin Joseph <avin.joseph@experionglobal.com>
2021-11-29 12:51:03 +0530

    commit appoinment

---
+++ src/app/shared/appoinmenttypes.ts
@@ -0,0 +1,4 @@
+export class Appoinmenttypes {
+    AppoinmentTypeId: number;
+    AppoinmentType: string;
+}
--- src/app/shared/appointment.service.ts
+++ src/app/shared/appointment.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Appointment } from './appointment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor() { }
+  appointments: Appointment[];
+  constructor(private httpClient: HttpClient) { }
+
+
+  //Add appointment i.e insert appointment
+  InsertAppoinment(appointment: Appointment):Observable<any>{
+    return this.httpClient.post(
+      environment.apiUrl + '/api/appointment', appointment
+    )
+  }
+
+  //Update Appointment 
+  UpdateAppointment(appointment: Appointment):Observable<any>{
+    return this.httpClient.put(
+      environment.apiUrl + '/api/appointment', appointment
+    );
+  }
+
+  //View all appointments
+  GetAllAppoinments(){
+    this.httpClient.get(environment.apiUrl + '/api/appointment').toPromise()
+    .then((response) => (this.appointments = response as Appointment[]));
+  }
+
+  //View all appointments by date
+  GetAllAppointmentsByDate(appDate: Date): Observable<any> {
+    return this.httpClient.get(environment.apiUrl + '/api/appointment/'+ appDate);
+  }
+
+  //view all appointments by doctor
+  GetAllAppointmentsByDoctor(empId: number): Observable<any> {
+    return this.httpClient.get(environment.apiUrl + '/api/appointment/'+ empId);
+  }
 }

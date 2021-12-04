22d2276b8f5568d98eb6f3f5dafd67b0c91037ed
vyshnav.sasidharan <vyshnav.sasidharan@VYSHNAVSASIDHAR>
2021-12-04 13:58:10 +0530

    lab test added

--- src/app/prescription/prescription.component.ts
+++ src/app/prescription/prescription.component.ts
@@ -1,7 +1,6 @@
-import { Test } from './../shared/test';
 import { PrescriptionService } from './../shared/prescription.service';
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm,FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
@@ -18,59 +17,93 @@ export class PrescriptionComponent implements OnInit {
   currentDate: Date = new Date();
   atId: number;
   form: FormGroup;
-  tests: string;
   data: any;
-  checkArray: FormArray;
-  Tests = [new Test(1, 'RTPCR'), new Test(2, 'HB'), new Test(3, 'Creatine')];
-  selectedTests: Test[];
+  addLabTests:boolean = false;
+
+  CountryData: Array<any> = [
+    { name: 1, value: 'ECG' },
+    { name: 2, value: 'MRI' },
+    { name: 3, value: 'X-Ray' },
+    { name: 4, value: 'BP' },
+    { name: 5, value: 'Urine' }
+  ];  
+
+  
   constructor(
     public preService: PrescriptionService,
     private toastr: ToastrService,
     private router: Router,
     private route: ActivatedRoute,
-    public appService: AppointmentService
-  ) {}
+    public appService: AppointmentService,
+    private fb: FormBuilder
+  ) {
+
+    this.form = this.fb.group({
+      checkArray: this.fb.array([])
+    })
+  }
 
   ngOnInit(): void {
-    console.log(this.Tests);
+  
     //get empId from activated route
     this.empId = this.route.snapshot.params['empId'];
     this.patientId = this.route.snapshot.params['patientId'];
     this.atId = this.route.snapshot.params['atId'];
     console.log(this.empId);
     console.log(this.patientId);
-    //this.empId = 2;
+    this.addLabTests = false;
     //this.patientId = 2;
   }
 
   onCheckboxChange(e) {
-    this.checkArray = this.form.get('checkArray') as FormArray;
-
+    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+  
     if (e.target.checked) {
-      this.checkArray.push(new FormControl(e.target.value));
-      console.log(this.checkArray);
+      checkArray.push(new FormControl(e.target.value));
     } else {
       let i: number = 0;
-
-      this.checkArray.controls.forEach((item: FormControl) => {
+      checkArray.controls.forEach((item: FormControl) => {
         if (item.value == e.target.value) {
-          this.checkArray.removeAt(i);
-
+          checkArray.removeAt(i);
           return;
         }
-
         i++;
       });
+    }
+  }
+
+  onAddLabTests(e) {
+  
+    if (e.target.checked) {
+      this.addLabTests = true;
+    } else {
+      this.addLabTests = false;
     }
   }
 
   //onSubmit function
   onSubmit(form: NgForm) {
     console.log(form.value);
-    this.selectedTests = form.controls['selectedTests'].value;
-    console.log(this.selectedTests);
+
     let addId = this.preService.prescriptionForm.PrescriptionId;
-    //insert
+    let jsonString : String = '';
+
+    if(this.addLabTests){
+      const checkArray: FormArray = this.form.get('checkArray') as FormArray;
+    let tests = [];
+    checkArray.controls.forEach((item: FormControl) => {
+
+      tests.push(item.value);
+    });
+
+    let josnData : any = {
+      "tests": tests
+    }
+
+    jsonString= JSON.stringify(josnData);
+    form.value.Tests = jsonString;
+    }
+
     if (addId == 0 || addId == null) {
       this.insertPrescription(form);
     }
@@ -104,14 +137,19 @@ export class PrescriptionComponent implements OnInit {
     let formatDate: any = datePipe.transform(this.currentDate, 'yyyy-MM-dd');
     form.value.PrescriptionDate = formatDate;
     console.log(form.value);
-    if (confirm('Do you want to add lab tests?')) {
-      this.data = { name: this.checkArray };
-      console.log(this.data);
-      this.tests = JSON.stringify(this.data);
-      console.log(this.tests);
-      //this.router.navigate(['labtest', this.patientId, this.empId]);
-    } else {
-      form.value.Tests = null;
+    if(!this.addLabTests){
+      if (confirm('Do you want to add lab tests?')) {
+
+      }else{
+    
+        this.preService.AddPrescription(form.value).subscribe((data) => {
+          console.log(data);
+          this.toastr.success('Prescription added', 'CMSApp v2021');
+        });
+        this.updateStatus(this.patientId);
+        this.router.navigate(['doctor', this.empId]);
+      } 
+    }else{
       this.preService.AddPrescription(form.value).subscribe((data) => {
         console.log(data);
         this.toastr.success('Prescription added', 'CMSApp v2021');
@@ -119,5 +157,6 @@ export class PrescriptionComponent implements OnInit {
       this.updateStatus(this.patientId);
       this.router.navigate(['doctor', this.empId]);
     }
+
   }
 }
